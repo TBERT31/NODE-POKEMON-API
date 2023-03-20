618609578f432448d57cf5ff80ec6303506052cc
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-20 05:46:15 +0100

    résolution bug pokemon

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-//console.log('Hello, Node ! 👋');
 const express = require('express');
+let pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Express ! 👋'));
+
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = req.params.id;
+    const pokemon = pokemons.find(pokemon => pokemon.id === id);
+    res.send(`Vous avez demandé le pokémon n°${pokemon.name}`);
+
+});
+
+
+
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : https://localhost:${port}`));
 
