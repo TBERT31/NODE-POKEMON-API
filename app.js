f32bcb678387e3902c18cda51b898fc073b78dcd
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-20 06:44:37 +0100

    get all pokemons

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { success } = require('./helper');
 let pokemons = require('./mock-pokemon');
 
 const app = express();
@@ -6,15 +7,19 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Express ! 👋'));
 
+
+app.get('/api/pokemons', (req,res) => {
+    const message = "La liste des pokémons a bien été récupérée.";
+    res.json(success(message, pokemons));
+});
+
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find(pokemon => pokemon.id === id);
-    res.json(pokemon);
+    const message = 'Un pokémon a bien été trouvé.'
+    res.json(success(message, pokemon));
 });
 
-app.get('/api/pokemons', (req,res) => {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment.`);
-});
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
 
