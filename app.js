3b8a471426b1037ce76394eff0ab27343eda3f2f
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-22 08:01:06 +0100

    tri asc

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require('./src/routes/deletePokemon')(app);
 app.use(({res}) => {
     const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.';
     res.status(404).json({message});
-})
+});
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
 
--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,33 @@
-const { Pokemon } = require('../db/sequelize')
+const { Pokemon } = require('../db/sequelize');
+const { Op } = require("sequelize");
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
-      .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
-      })
-      .catch(error => {
-        const message = `La liste des pokémons n'a pas pu être récupérée. Réessayer dans quelques instants.`;
-        res.status(500).json({message, data: error});
-      })
+    if(req.query.name){
+        const name = req.query.name;
+        return Pokemon.findAndCountAll({
+            where: {
+                name: { // 'name' est la propriété du modèle pokemon
+                    [Op.like]: `%${name}%` // 'name' est le critère de la recherche
+                }
+            },
+            order: ['name'],
+            limit: 5
+        })
+        .then(({count, rows}) => {
+            const message = `La y a ${count} pokémons qui correspondent au terme de recherche ${name}.`;
+            res.json({ message, data: rows })
+        })
+    } else {
+        Pokemon.findAll({order: ['name']})
+        .then(pokemons => {
+          const message = 'La liste des pokémons a bien été récupérée.'
+          res.json({ message, data: pokemons })
+        })
+        .catch(error => {
+          const message = `La liste des pokémons n'a pas pu être récupérée. Réessayer dans quelques instants.`;
+          res.status(500).json({message, data: error});
+        })
+    }
   })
 }
