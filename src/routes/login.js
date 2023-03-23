80216cb656ca9e0a0e7ef02959c36b9bf42f2f4f
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-23 06:54:48 +0100

    Fin sécurité et authentification avec JWT

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,8 @@
-const { User } = require('../db/sequelize')
-const bcryptjs = require('bcryptjs')
-  
+const { User } = require('../db/sequelize');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const privateKey = require('../auth/private_key');
+
 module.exports = (app) => {
   app.post('/api/login', (req, res) => {
     User.findOne({ where: { username: req.body.username } }).then(user => {
@@ -15,8 +17,15 @@ module.exports = (app) => {
           return res.status(401).json({ message})
         }
 
+        // JWT
+        const token = jwt.sign(
+            {userId: user.id},
+            privateKey,
+            {expiresIn: '24h'}
+        );
+
         const message = `L'utilisateur a été connecté avec succès.`;
-        return res.json({ message, data: user })
+        return res.json({ message, data: user, token })
       })
     })
     .catch(error => {
